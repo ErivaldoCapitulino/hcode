83ed6cdb846bfd9b2c45574f1a1c72bed7e6ae31
Erivaldo Capitulino <vadositi@gmail.com>
2023-01-29 12:47:11 -0300

    implementing date and time the time was implemented in the setInterval() method

---
+++ calculadora/README.md
@@ -0,0 +1,26 @@
+# JavaScript Calculator
+
+project carried out in the **_javascript course - Complete course with 6 real [HCODE](https://www.hcode.com.br/) projects_**, in this project I was able to put into practice the following concepts:
+
+- MVC
+- Object Orientation
+- Constructor and encapsulation
+- Manipulate the DOM
+- Work with date and time
+- QuerySelector
+- Click event
+- Event using split()
+- switch
+- Operations - inNaN and Array.length
+- Work with audio
+- Error Handling
+
+#### Project
+
+![Calculator](./img/calculator.png)
+
+#### Used Technologies
+
+![HTML](./img/filetype-html.svg) html
+![CSS](./img/filetype-css.svg) css
+![JAVASCRIPT](./img/filetype-js.svg) javascript
---
+++ calculadora/controller/CalcController.js
@@ -0,0 +1,57 @@
+class CalcController {
+  constructor() {
+    this._locale = "pt-BR";
+    this._displayCalcEl = document.querySelector("#display");
+    this._dateEl = document.querySelector("#data");
+    this._timeEl = document.querySelector("#hora");
+    this.currentDate;
+    this.initialize();
+  }
+
+  initialize() {
+    this.setDisplayTime();
+    setInterval(() => {
+      this.setDisplayTime();
+    }, 1000);
+  }
+
+  setDisplayTime() {
+    this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+    this.displayDate = this.currentDate.toLocaleDateString(this._locale, {
+      day: "2-digit",
+      month: "long",
+      year: "numeric",
+    });
+  }
+
+  get displayDate() {
+    return this._dateEl.innerHTML;
+  }
+
+  set displayDate(value) {
+    this._dateEl.innerHTML = value;
+  }
+
+  get displayTime() {
+    return this._timeEl.innerHTML;
+  }
+
+  set displayTime(value) {
+    this._timeEl.innerHTML = value;
+  }
+
+  get displayCalcEl() {
+    return this._displayCalcEl.innerHTML;
+  }
+
+  set displayCalcEl(value) {
+    this._displayCalcEl.innerHTML = value;
+  }
+
+  get currentDate() {
+    return new Date();
+  }
+  set currentDate(value) {
+    this.currentDate = value;
+  }
+}
